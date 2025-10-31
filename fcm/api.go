f624e9cceb2223d879b5895ea692cb4a6df3827a
Tarab1te <69683068+JayyDoesDev@users.noreply.github.com>
2025-10-31 15:40:34 -0400

    Update api.go

--- fcm/api.go
+++ fcm/api.go
@@ -5,3 +5,7 @@ import "github.com/jayydoesdev/gokalshi"
 func GetFCMOrders(keyID, keyPem string, q FCMOrdersQuery) ([]byte, error) {
 	return gokalshi.Request[[]byte]("/fcm/orders", "GET", keyID, keyPem, true, q.ToMap())
 }
+
+func GetFCMPositions(keyID, keyPem string, q FCMPositionsQuery) ([]byte, error) {
+	return gokalshi.Request[[]byte]("/fcm/positions", "GET", keyID, keyPem, true, q.ToMap())
+}
